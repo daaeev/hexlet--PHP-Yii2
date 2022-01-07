953b0d5415b8b81f8b3fae1e34d436640ec0dab1
daaeev <LfdblGbhju@gmail.com>
2022-01-07 21:36:39 +0200

    Добавлена библиотека ParseDown для работы с маркдаун разметкой

--- web/associate.php
+++ web/associate.php
@@ -4,8 +4,14 @@ use app\components\helpers\interface\RoleHelperInterface;
 use app\components\helpers\RoleHelper;
 use yii\rbac\DbManager;
 use yii\rbac\ManagerInterface;
+use Parsedown;
 
 $container = Yii::$container;
 
 $container->setSingleton(RoleHelperInterface::class, RoleHelper::class);
 $container->setSingleton(ManagerInterface::class, DbManager::class);
+
+$markDownParser = new Parsedown;
+$markDownParser->setSafeMode(true);
+
+$container->set(Parsedown::class, $markDownParser);
